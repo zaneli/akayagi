2362f96b92131e6017f6607340655c2a5be8c154
zaneli <shun.otani@gmail.com>
2012-11-05 12:35:21 +0900

    correct the migration file

--- db/migrate/002_create_akayagi_project_settings.rb
+++ db/migrate/002_create_akayagi_project_settings.rb
@@ -1,7 +1,7 @@
 class CreateAkayagiProjectSettings < ActiveRecord::Migration
   def change
     create_table :akayagi_project_settings do |t|
-      t.integer :project_id, null => false
+      t.integer :project_id, :null => false
       t.string :subject_prefix
       t.string :from_address
     end
